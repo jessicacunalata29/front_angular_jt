fce9c1081ec6326065ef93208518ce942d2bd150
OmarJB20 <ojc.santillan@yavirac.edu.ec>
2025-11-12 07:56:59 -0500

    Primer commit del proyecto

---
+++ src/app/web/nosotros/nosotros.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nosotros',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      nosotros works!
+    </p>
+  `,
+  styles: ``
+})
+export class NosotrosComponent {
+  public titulo: string = "Acerca de nosotros"
+
+}
